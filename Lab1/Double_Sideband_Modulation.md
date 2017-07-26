255fec8b8d482e641c7176dda32cb7e1648ab5df
Pranav <pranavsankh40@gmail.com>
2017-07-27 02:20:58 +0530

    Lab1

---
+++ Lab1/Double_Sideband_Modulation.md
@@ -0,0 +1,16 @@
+# Double Sideband Modulation
+
+This translation of spectrum of the “message signal" from baseband to a carrier frequency is called up-conversion or carrier modulation. 
+
+- As observed in your simulation, there are two tones: One at 9.5kHz and another one at 10.5kHz
+- These are called the two sidebands of the signal
+
+## A variation of DSM : **DSB-SC** 
+
+Double-sideband suppressed-carrier transmission (DSB-SC) is transmission in which frequencies produced by amplitude modulation (AM) are symmetrically spaced above and below the carrier frequency and the carrier level is reduced to the lowest practical level, ideally being completely suppressed.
+
+In the DSB-SC modulation, unlike in AM, the wave carrier is not transmitted; thus, much of the power is distributed between the sidebands, which implies an increase of the cover in DSB-SC, compared to AM, for the same power used.
+
+DSB-SC transmission is a special case of double-sideband reduced carrier transmission. It is used for radio data systems.
+
+**Note:**  DSB-SC is basically an amplitude modulation wave without the carrier, therefore reducing power waste, giving it a 100% efficiency. This is an increase compared to normal AM transmission (DSB), which has a maximum efficiency of 33.333%, since 2/3 of the power is in the carrier which carries no intelligence, and each sideband carries the same information. Single Side Band (SSB) Suppressed Carrier is 100% efficient.
---
+++ Lab1/getting_started.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Getting Started
+# Generated: Wed Jul 26 23:03:06 2017
+##################################################
+
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import wx
+
+class getting_started(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Getting Started")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.lab1exp1 = self.lab1exp1 = wx.Notebook(self.GetWin(), style=wx.NB_TOP)
+        self.lab1exp1.AddPage(grc_wxgui.Panel(self.lab1exp1), "scope")
+        self.lab1exp1.AddPage(grc_wxgui.Panel(self.lab1exp1), "fft")
+        self.Add(self.lab1exp1)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
+        	self.lab1exp1.GetPage(0).GetWin(),
+        	title="Scope Plot",
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_NORM,
+        	y_axis_label="Counts",
+        )
+        self.lab1exp1.GetPage(0).Add(self.wxgui_scopesink2_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_f(
+        	self.lab1exp1.GetPage(1).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.lab1exp1.GetPage(1).Add(self.wxgui_fftsink2_0.win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate)
+        self.audio_sink_0 = audio.sink(samp_rate, "", True)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, 500, 1, 0)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_scopesink2_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.audio_sink_0, 0))
+
+
+# QT sink close method reimplementation
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+
+if __name__ == '__main__':
+    import ctypes
+    import os
+    if os.name == 'posix':
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = getting_started()
+    tb.Start(True)
+    tb.Wait()
+
